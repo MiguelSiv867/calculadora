e9b474d8aa15031901ff50c3439b6386dd351818
miguel <miguelaquinez17@gmail.com>
2025-06-16 17:54:01 -0300

    calculadora

---
+++ calculadora.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main (){
+	char r;
+	int n1;
+	int n2;
+	int valor;
+	printf("Escolha qual operação você quer fazer:\n1-Multiplicação\n2-Subtração\n3-Divisão\n4-Adição\n");
+	scanf("%c", &r);
+	if (r = 1){
+	printf("Digite o primeiro número:\n");
+	scanf("%d", & n1);
+	printf("Digite o segundo número:\n");
+	scanf("%d", & n2);
+        valor = n1*n2 ;
+	printf("O Valor da Multiplicação é: %d " valor);
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
