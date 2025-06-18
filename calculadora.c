b0a4118d36bd66de09ee6de58a01d23ca2bc28cd
MiguelSiv867 <miguel.silva294@etec.sp.gov.br>
2025-06-18 20:03:17 -0300

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,37 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main (){
-	char r;
-	int n1;
-	int n2;
-	int valor;
-	printf("Escolha qual operação você quer fazer:\n1-Multiplicação\n2-Subtração\n3-Divisão\n4-Adição\n");
-	scanf("%c", &r);
-	if (r = 1){
-	printf("Digite o primeiro número:\n");
-	scanf("%d", & n1);
-	printf("Digite o segundo número:\n");
-	scanf("%d", & n2);
-        valor = n1*n2 ;
-	printf("O Valor da Multiplicação é: %d " valor);
 
-	}
+
+void main(){
+    
+int ope;    
+int va1, va2;
+int resu;
+char r = 'q';
+float v1;
+float v2;
+float res;
 
 
 
+while (r == 'q'|| r == 'Q'){
 
 
+printf("escolha uma das quatro operações\n\n1.soma\n2.subtração\n3.multiplicação\n4.divisão\n");
+scanf("%d", &ope);
 
-
-
-
-
-
-
-
-
-
-
+if (ope == 1) {
+    printf("digite um numero:\n");
+    scanf("%d", &va1);
+    printf("digte mais um numero:\n");
+    scanf("%d", &va2);
+    resu = va1 + va2;
+    printf("resultado: %d\n", resu);
+    
+}else if (ope == 2) {
+    printf("digite um numero:\n");
+    scanf("%d", &va1);
+    printf("digte mais um numero:\n");
+    scanf("%d", &va2);
+    resu = va1 - va2;
+    printf("resultado: %d\n", resu);
+    
+}else if (ope == 3) {
+    printf("digite um numero:\n");
+    scanf("%d", &va1);
+    printf("digte mais um numero:\n");
+    scanf("%d", &va2);
+    resu = va1 * va2;
+    printf("resultado: %d\n", resu);
+    
+}else if (ope == 4) {
+    printf("digite um numero:\n");
+    scanf("%f", &v1);
+    printf("digte mais um numero:\n");
+    scanf("%f", &v2);
+    res = v1 / v2;
+    printf("resultado: %.2f\n", res);
+    
 
 }
+
+
+printf ("se deseja realizar outras operações digite (Q):\n");
+scanf (" %c", &r);
+
+}
+
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
